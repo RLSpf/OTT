5d2c010b0ed8afaa5c92d6bed740bb133b8695e0
RLSpf <61246456+RLSpf@users.noreply.github.com>
2020-02-19 17:56:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ bot.on('disconnect', e => {
   console.log('Bot disconnected:', e)
 })
 
-bot.login(token)
+bot.login(process.env.token);
+Njc5NzQ2NTQ0OTY2MDQxNjAx.Xk11Yw.V1wOP13zCX3PmqSYO8gQC_eLauw
 
 module.exports = bot
